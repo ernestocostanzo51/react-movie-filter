c5913d39ad4b120b785aa8e2c9bdfcb7df2b1547
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-16 14:32:17 +0100

    creazione lista film

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,42 @@ import { useState } from 'react'
 
 function App() {
  
+  const films = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
+
+  
 
   return (
     <>
-     <h1>PROVA PROVA</h1>
-     <p>prova</p>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1>Lista film</h1>
+        </div>
+      </div>
+    </div>
+     <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <ul className="list-group">
+            <h3>Titoli</h3>
+            {
+              films.map(film =>(
+                <li className='list-group-item'>Titolo: {film.title} Genere:{film.genre}</li>
+              ))
+            }
+          </ul>
+        </div>
+         
+        
+      </div>
+     </div>
     </>
   )
 }
